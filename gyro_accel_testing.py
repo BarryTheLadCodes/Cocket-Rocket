f7097a43bf9d66bd7ebd55a849b36d2f554562a0
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-01-28 18:59:20 -0800

    stupid typo

--- gyro_accel_testing.py
+++ gyro_accel_testing.py
@@ -39,7 +39,7 @@ def angles():
     roll_angle_accelerometer = math.degrees(math.atan2(accelerometer_x, math.sqrt(accelerometer_y**2 + accelerometer_z**2)))
 
     #Combine accelerometer and gyroscope data for final angle
-    pitch_angle = alpha * (pitch_angle + pitch_data * delay) + (1 - alpha) * pitch_angle_accelerometer()
+    pitch_angle = alpha * (pitch_angle + pitch_data * delay) + (1 - alpha) * pitch_angle_accelerometer
     roll_angle = alpha * (roll_angle + roll_data * delay) + (1 - alpha) * roll_angle_accelerometer
 
     #Integrate yaw angular velocity with time to get yaw angle approximation
