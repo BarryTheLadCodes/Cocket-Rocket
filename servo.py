34aa155ae5d9d6052333d011247be9fda59dded1
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-03-13 20:50:09 -0700

    trying with pigpio

--- servo.py
+++ servo.py
@@ -1,26 +1,23 @@
-import RPi.GPIO as GPIO # type: ignore
+import pigpio # type: ignore
 import time
 
 # Set pin 21 as servo1 output
-SERVO_1_PIN = 40
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(SERVO_1_PIN, GPIO.OUT)
+SERVO_1_PIN = 21
+servo_1 = pigpio.pi()
 
-# Setup PWM at 50Hz
-SERVO_1 = GPIO.PWM(SERVO_1_PIN, 50)
-SERVO_1.start(0)
+if not servo_1.connected:
+    print("Failed to connect to pigpio daemon!")
+    exit()
+
+servo_1.set_PWM_frequency(SERVO_1_PIN, 50)  # Set frequency to 50Hz
+servo_1.set_servo_pulsewidth(SERVO_1_PIN, 1500) # Set initial pulse width to 1.5ms (neutral position)
 
 def servo_write_angle(angle):
-    # Assuming a 50Hz PWM frequency (20ms period)
-    # 1ms pulse width corresponds to 5% duty cycle
-    # 2ms pulse width corresponds to 10% duty cycle
-    min_duty = 5
-    max_duty = 10
+    min_pulse = 1000
+    max_pulse = 2000
     # Convert angle from -90 to 90 degrees to 0 to 180 degrees
     angle = angle + 90
-    duty_cycle = min_duty + (angle / 180.0) * (max_duty - min_duty)
-    SERVO_1.ChangeDutyCycle(duty_cycle)
-
+    servo_1.set_servo_pulsewidth(SERVO_1_PIN, (angle / 180) * (max_pulse - min_pulse) + min_pulse)
 while True:
     servo_write_angle(-90)
     time.sleep(1)
