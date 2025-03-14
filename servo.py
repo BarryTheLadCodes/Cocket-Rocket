1da81fffce990db9f151f390bf50a198c6c11469
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-03-13 20:16:36 -0700

    test

--- servo.py
+++ servo.py
@@ -4,11 +4,19 @@ import time
 # Set pin 21 as servo1 output
 SERVO_1_PIN = 21
 servo_1 = AngularServo(SERVO_1_PIN, min_angle=-90, max_angle=90)
+#while True:
+#    angle = -90
+#    servo_1.angle = angle
+#    time.sleep(0.5)
+#    for n in range (18):
+#        angle += 10
+#        servo_1.angle = angle
+#        time.sleep(0.5)
+
 while True:
-    angle = -90
-    servo_1.angle = angle
-    time.sleep(0.5)
-    for n in range (18):
-        angle += 10
-        servo_1.angle = angle
-        time.sleep(0.5)
+    servo_1.angle = -90
+    time.sleep(2)
+    servo_1.angle = 0
+    time.sleep(2)
+    servo_1.angle = 90
+    time.sleep(2)
