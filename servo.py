3e9789a585ed3400f8f064d39c7bcaf4e2bd08ea
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-03-13 20:25:14 -0700

    changed to pigpiofactory

--- servo.py
+++ servo.py
@@ -1,9 +1,9 @@
-from gpiozero import AngularServo  # type: ignore
+from gpiozero import AngularServo, PiGPIOFactory  # type: ignore
 import time
 
 # Set pin 21 as servo1 output
 SERVO_1_PIN = 21
-servo_1 = AngularServo(SERVO_1_PIN, min_angle=-90, max_angle=90)
+servo_1 = AngularServo(SERVO_1_PIN, min_angle=-90, max_angle=90, pin_factory=PiGPIOFactory())
 #while True:
 #    angle = -90
 #    servo_1.angle = angle
