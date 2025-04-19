5ed8e2ca6d0dbe5b9461c5daabd0f6ef041aae3f
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-04-18 20:32:55 -0700

    testing different pulse length

--- servo.py
+++ servo.py
@@ -19,8 +19,8 @@ servo_Y.set_PWM_frequency(SERVO_Y_PIN, 50)  # Set frequency to 50Hz
 servo_Y.set_servo_pulsewidth(SERVO_Y_PIN, 1500) # Set initial pulse width to 1.5ms (neutral position)
 
 def servo_write_angle(angle, servo):
-    min_pulse = 1000
-    max_pulse = 2000
+    min_pulse = 500
+    max_pulse = 2500
     # Convert angle from -90 to 90 degrees to 0 to 180 degrees
     if servo == "X":
         angle = angle + SERVO_X_OFFSET + 90
