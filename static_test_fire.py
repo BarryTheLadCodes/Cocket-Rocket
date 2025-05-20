9084db27deb9a243b51992b20b23747f9d366f6b
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-05-19 21:16:57 -0700

    testing static fire test code

--- static_test_fire.py
+++ static_test_fire.py
@@ -5,12 +5,12 @@ while True:
     for i in range(3):
         servo.servo_write_angle(-10, "X")
         servo.servo_write_angle(-10+i*10, "Y")
-        time.sleep(0.1)
+        time.sleep(3)
     for i in range(3):
         servo.servo_write_angle(0, "X")
         servo.servo_write_angle(10-i*10, "Y")
-        time.sleep(0.1)
+        time.sleep(3)
     for i in range(3):
         servo.servo_write_angle(10, "X")
         servo.servo_write_angle(-10+i*10, "Y")
-        time.sleep(0.1)
+        time.sleep(3)
