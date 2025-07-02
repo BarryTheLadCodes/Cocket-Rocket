a0cca7705b1aea2e01de02fc5f6169ae2c005b86
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-07-01 22:46:09 -0700

    added new servo offset

--- servo.py
+++ servo.py
@@ -4,8 +4,8 @@ import time
 # Set pin 21 as servo1 output
 SERVO_X_PIN = 20
 SERVO_Y_PIN = 21
-SERVO_X_OFFSET = 0
-SERVO_Y_OFFSET = 0
+SERVO_X_OFFSET = -17
+SERVO_Y_OFFSET = -18
 servo_X = pigpio.pi('localhost', 8888)
 servo_Y = pigpio.pi('localhost', 8888)
 
